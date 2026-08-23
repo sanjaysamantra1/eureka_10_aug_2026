c9b7cf2d989b3ccbfb507fa93f67dc5e13bb149f
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-23 06:19:53 +0530

    day 12

---
+++ javascript/day_12/math.js
@@ -0,0 +1,11 @@
+
+export function add(a, b) {
+  console.log(`Addition of ${a} & ${b} is ${a + b}`);
+}
+export function sub(a, b) {
+  console.log(`Subtraction of ${a} & ${b} is ${a - b}`);
+}
+export function mul(a, b) {
+  console.log(`Multiplication of ${a} & ${b} is ${a * b}`);
+}
+
---
+++ javascript/day_12/math_test.js
@@ -0,0 +1,5 @@
+import { add, sub, mul } from "./math.js";
+
+add(10, 20);
+sub(10, 20);
+mul(10, 20);
