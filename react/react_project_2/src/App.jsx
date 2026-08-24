f6b3664460e5edb3a3289438da5a096e878512f8
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-24 07:38:06 +0530

    react - day 1

---
+++ react/react_project_2/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Categories from "./components/categories/Categories";
+import Carousel from "./components/carousel/Carousel";
+import Body from "./components/body/Body";
+import Footer from "./components/footer/Footer";
+
+export default function App() {
+  return (
+    <div>
+      {/* <h3>This is App Component</h3> */}
+      <Navbar />
+      <Categories />
+      <Carousel />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
---
+++ react/react_project_2/src/components/categories/Categories.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Categories() {
+  return <div className='container'>
+    <div className="row">
+      <div className="col">
+        <img src="https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/fashion.svg" alt="" />
+        <div>Fashion</div>
+      </div>
+      <div className="col">
+        <img src="https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/mobiles.svg" alt="" />
+        <div>Mobiles</div>
+      </div>
+      <div className="col">
+        <img src="https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/electronics.svg" alt="" />
+        <div>Electronics</div>
+      </div>
+      <div className="col">
+        <img src="https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/home.svg" alt="" />
+        <div>Home</div>
+      </div>
+      <div className="col">
+        <img src="https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/tv.svg" alt="" />
+        <div>Appliances</div>
+      </div>
+      <div className="col">
+        <img src="https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/furniture.svg" alt="" />
+        <div>Furnitures</div>
+      </div>
+      <div className="col">
+        <img src="https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/food.svg" alt="" />
+        <div>Food</div>
+      </div>
+      <div className="col">
+        <img src="https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/auto-new.svg" alt="" />
+        <div>2 wheeler</div>
+      </div>
+      <hr/>
+    </div>
+  </div>
+}
