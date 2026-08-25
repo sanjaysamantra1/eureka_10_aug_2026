8daa71b1846d4858413206028dc246802b3943ce
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-08-25 06:34:29 +0530

    footer

--- react/react_project_2/src/components/footer/Footer.jsx
+++ react/react_project_2/src/components/footer/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export default function Footer() {
-  return <footer class="py-3 my-4">
+  return <footer class="py-3 my-4 bg-dark text-white">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2">About</a></li>
     </ul>
-    <p class="text-center text-body-secondary">© 2025 Company, Inc</p>
+    <p class="text-center">© 2025 Company, Inc</p>
   </footer>
 }
