26d649a8b1268b312d56a291497faca28d9d8990
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-09-08 07:11:54 +0530

    validation

--- react/react_project_2/src/components/form/FormDemo1.jsx
+++ react/react_project_2/src/components/form/FormDemo1.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react"
 
-export default function FormDemo1() {
+export default function FormDemo1() { // Controlled Form
     const initialData = {
+        firstName: 'Virat',
+        lastName: '',
+        email: '',
+        gender: ''
+    }
+    const initialError = {
         firstName: '',
         lastName: '',
-        email: ''
+        email: '',
+        gender: ''
     }
     const [user, setUser] = useState(initialData)
+    const [errors, setErrors] = useState(initialError)
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -19,6 +27,10 @@ export default function FormDemo1() {
     const handleChange = (event) => {
         const { name, value } = event.target;
         setUser({ ...user, [name]: value })
+        setErrors({
+            ...errors,
+            [name]: value.trim() === "" ? `${name} is required` : "",
+        });
     }
     return <>
         <h3 className="text-center">Form Demo 1</h3>
@@ -26,8 +38,9 @@ export default function FormDemo1() {
         <div className="col-sm-4 offset-4 border border-3 rounded-3 p-3">
             <form onSubmit={handleSubmit} onReset={handleReset}>
                 <p className="my-1">
-                    FirstName:
-                    <input name="firstName" value={user.firstName} onChange={handleChange} />
+                    <label htmlFor="firstName">FirstName:</label>
+                    <input id="firstName" name="firstName" value={user.firstName} onChange={handleChange} />
+                    {errors.firstName ? <span className="text-danger">FirstName is Required</span> : null}
                 </p>
                 <p className="my-1">
                     lastName:
@@ -37,13 +50,21 @@ export default function FormDemo1() {
                     email:
                     <input name="email" value={user.email} onChange={handleChange} />
                 </p>
+                <p className="my-1">
+                    Gender: &nbsp;
+                    <input id="male" name="gender" type="radio" value="male" checked={user.gender == 'male'} onChange={handleChange} />
+                    <label htmlFor="male">Male</label>
+                    &nbsp;
+                    <input id="female" name="gender" type="radio" value="female" checked={user.gender == 'female'} onChange={handleChange} />
+                    <label htmlFor="female">Female</label>
+                </p>
 
                 <button type="reset">Reset</button>
                 <button type="submit" className="mx-2">Submit</button>
             </form>
 
             <pre>
-                {JSON.stringify(user,null,2)}
+                {JSON.stringify(user, null, 2)}
             </pre>
         </div>
     </>
